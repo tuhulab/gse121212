e9acc8201b198b8c00b43b1d412150af12c2012f
Tu Hu <UYHDK@leo-pharma.com>
2020-07-29 17:23:53 +0200

    Update sanity_check.R

--- R/sanity_check.R
+++ R/sanity_check.R
@@ -1,4 +1,7 @@
 library(dplyr)
 
 fastq <- readr::read_csv("ids/fastq.id", col_names = FALSE)
-fastq_srr_is <- fastq$X1
+fastq_srr_id <- fastq$X1 %>% stringr::str_extract("SRR\\d{1,}") %>% unique()
+
+all_srr_id <- readr::read_csv("ids/SRR_Acc_List.txt", col_names = FALSE) %>% pull(X1)
+all_srr_id[!(all_srr_id %in% fastq_srr_id)] %>% data.frame() %>% readr::write_csv("ids/missing_srr_id.txt", col_names = FALSE)
