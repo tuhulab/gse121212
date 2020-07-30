a1b69057222cb7af52c0ef262f37f4c5d4fcd719
Tu Hu <tu.hu@outlook.com>
2020-07-30 09:01:40 +0200

    data_processing_script

--- doc/data-processing.sh
+++ doc/data-processing.sh
@@ -4,25 +4,29 @@
 module load sratoolkit/2.9.6-1
 prefetch --option-file /home/people/tuhu/Downloads/SRR_Acc_List.txt
 
-# download data
+# fasterq-dump data
 cd /home/projects/ku_00015/data/sratool_data/sra
 module load parallel/20200522 sratoolkit/2.9.6-1
 cat /home/people/tuhu/Downloads/SRR_Acc_List.txt | parallel "fasterq-dump /home/projects/ku_00015/data/sratool_data/sra/{}.sra"
+
+# download missing data
+module load sratoolkit/2.10.7 parallel/20200522
+prefetch --option-file /home/projects/ku_00015/people/tuhu/gse121212/ids/missing_srr_id.txt
+cd /home/projects/ku_00015/data/sratool_data/sra
+cat /home/projects/ku_00015/people/tuhu/gse121212/ids/missing_srr_id.txt | parallel "fasterq-dump /home/projects/ku_00015/data/sratool_data/sra/{}.sra"
 
 # load module
 echo "Step 0 - Load modules".
 module load tools fastp/0.19.4 parallel/20200522 anaconda2/4.4.0 hisat2/2.2.0 subread/1.6.2
 
 # initiate - ids
-TEST_ID_PATH=/home/projects/ku_00015/people/tuhu/multiomics-ad-transcriptomics/data/ids/test_ids.txt
-ID_PATH=/home/projects/ku_00015/people/tuhu/multiomics-ad-transcriptomics/data/ids/ids_exclude87.txt
+ID_PATH=/home/projects/ku_00015/people/tuhu/gse121212/ids/ids.txt
 
 # QC(1) - fastqc
 echo "Step 0 - FastQC"
 module load perl/5.30.2 jdk/14 fastqc/0.11.8 parallel/20200522
-cat ${ID_PATH} |  parallel "fastqc /home/projects/ku_00015/data/rna-seq/read/{}_1.fastq.gz /home/projects/ku_00015/data/rna-seq/read/{}_2.fastq.gz -o /home/projects/ku_00015/data/rna-seq/qc/"
-cd /home/projects/ku_00015/data/rna-seq
-fastqc read/*.fastq -o qc/
+mkdir /home/projects/ku_00015/data/gse1212_data/qc
+cat ${ID_PATH} |  parallel "fastqc /home/projects/ku_00015/data/gse1212_data/reads/{}_1.fastq /home/projects/ku_00015/data/gse1212_data/reads/{}_2.fastq -o /home/projects/ku_00015/data/rna-seq/qc/"
 
 # QC(2) - multiqc
 module unload fastqc/0.11.8 perl/5.30.2
@@ -30,13 +34,11 @@ module load anaconda3/4.4.0
 cd /home/projects/ku_00015/data/rna-seq
 multiqc qc/*_fastqc.zip -o qc/
 
-# QC(3) - multi-multi QC
-
 # trimming
 echo "Step 1 - Trimming"
 mkdir -p /home/projects/ku_00015/data/rna-seq/trimmed/fastp
 mkdir -p /home/projects/ku_00015/data/rna-seq/trimmed/fastp-report
-cat ${ID_PATH} |  parallel "fastp -i /home/projects/ku_00015/data/rna-seq/read/{}_1.fastq.gz -I /home/projects/ku_00015/data/rna-seq/read/{}_2.fastq.gz -o /home/projects/ku_00015/data/rna-seq/trimmed/fastp/{}_1.fastq.gz -O /home/projects/ku_00015/data/rna-seq/trimmed/fastp/{}_2.fastq.gz -w 1 -h /home/projects/ku_00015/data/rna-seq/trimmed/fastp-report/{}-report.html"
+cat ${ID_PATH} |  parallel "fastp -i /home/projects/ku_00015/data/gse1212_data/reads/{}_1.fastq -I /home/projects/ku_00015/data/gse1212_data/reads/{}_2.fastq -o /home/projects/ku_00015/data/rna-seq/trimmed/fastp/{}_1.fastq.gz -O /home/projects/ku_00015/data/rna-seq/trimmed/fastp/{}_2.fastq.gz -w 1 -h /home/projects/ku_00015/data/rna-seq/trimmed/fastp-report/{}-report.html"
 
 # mapping
 echo "Step 2 - Mapping"
@@ -59,5 +61,3 @@ cat ${TEST_ID_PATH} | parallel "java -jar /services/tools/picard-tools/2.20.2/pi
 # feature counting
 mkdir -p /home/projects/ku_00015/data/rna-seq/counts
 featureCounts -T 32 -t exon -g gene_id -a /home/projects/ku_00015/data/rna-seq/reference/gtf/hg38.ensGene.gtf -o /home/projects/ku_00015/data/rna-seq/counts/counts.txt /home/projects/ku_00015/data/rna-seq/mapping/hisat2/*.bam
-
-# test one mapping tool
