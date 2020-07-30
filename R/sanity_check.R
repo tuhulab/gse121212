61cf285df1a08c2c351747655bf670827dc10c6a
TU HU <TU@NEXS.KU.DK>
2020-07-30 20:15:54 +0200

    Update sanity_check.R

--- R/sanity_check.R
+++ R/sanity_check.R
@@ -5,3 +5,5 @@ fastq_srr_id <- fastq$X1 %>% stringr::str_extract("SRR\\d{1,}") %>% unique()
 
 all_srr_id <- readr::read_csv("ids/SRR_Acc_List.txt", col_names = FALSE) %>% pull(X1)
 all_srr_id[!(all_srr_id %in% fastq_srr_id)] %>% data.frame() %>% readr::write_csv("ids/missing_srr_id.txt", col_names = FALSE)
+
+all_srr_id %>% paste0(".sra") %>% data.frame() %>% readr::write_csv("ids/ids.txt", col_names = FALSE)
